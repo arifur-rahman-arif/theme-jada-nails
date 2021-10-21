75e986ad8576a5590982c4368e66292a5584f977
Arifur Rahman Arif <dragonwarrior052@gmail.com>
2021-10-21 17:16:55 +0600

    changed version number

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ if (!defined('ABSPATH')) {
 }
 
 if (!defined('JADA_NAILS_VERSION')) {
-    define('JADA_NAILS_VERSION', '1.1.0');
+    define('JADA_NAILS_VERSION', '1.1.1');
     // define('JADA_NAILS_VERSION', time());
 }
 
